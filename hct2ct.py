fa861995a0d63f6a4c35861609173f434d28e317
Xiaoman896 <73009207+Xiaoman896@users.noreply.github.com>
2023-01-06 21:14:48 -0600

    Update hct2ct.py

--- hct2ct.py
+++ hct2ct.py
@@ -39,12 +39,12 @@ def hct2ct(Np, Nr, linear_step, pixel_size, path, mispixels, k1):
     I_virtal = Usedproj.min() * np.ones([Rows_virtual, np.shape(Rawproj)[1]])
 
     for kk in range (np.shape(k)[0]):
-        for i in range (np.shape(Usedproj)[0]-1):
+        for i in range (np.shape(Rawproj)[0]-1):
             i1 = int(i + D_travel[kk])
             if np.mod(np.floor((k[kk]) / (Np / 2)), 2) == 0:
                I_virtal[i1][:] = w[kk] * Usedproj[kk,i,:] + (1 - w[kk]) * Usedproj[kk,i + 1,:]
             else:
-                for j in range(np.shape(Usedproj)[1]):
+                for j in range(np.shape(Rawproj)[1]):
                     j1 = np.shape(Rawproj)[1] - 1 - j
                     if ((j1 + mispixels) >= 0) & ((j1 + mispixels) < np.shape(Rawproj)[1]):
                         I_virtal[i1][j1 + mispixels] = w[kk] * Usedproj[kk,i, j] + (1 - w[kk]) * Usedproj[kk,i + 1, j]
