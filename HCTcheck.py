ae128c595947196772806303298c942e69bc6843
Xiaoman896 <73009207+Xiaoman896@users.noreply.github.com>
2023-02-07 14:54:31 -0600

    Update HCTcheck.py

--- HCTcheck.py
+++ HCTcheck.py
@@ -1,42 +1,43 @@
 # helical converter development - version1.2: it is the HCT converter test/checking program
 # Author: Xiaoman Duan
 # Contact: xid896@usask.ca
-# Create date: 2022-12-07
-# Last edit date: 2023-01-06
-
+# Create date:  2022-12-20
+# Last edit date: 2023-02-07
 import shutil
 import os, time
 from hct2ct import hct2ct
 import tifffile
 import argparse
 
+parser = argparse.ArgumentParser(description='HCT2CT, for conversion from helical CT projection to CT projection for  parallel beam')
+parser.add_argument('-Np', type=int, default="3000", help='number of projection per rotation (360 degrees)')
+parser.add_argument('-Nr', type=int, default=3, help='number of rotation')
+parser.add_argument('-linear_step', type=float, default=0.001, help='linear step (i.e., traveling distance per projectio), uint of mm')
+parser.add_argument('-pixel_size', type=float, default=0.013, help='pixel size, uint of mm')
+parser.add_argument('-inputpath', type=str, default='tomo/', help='input folder path: HCT projections')
+parser.add_argument('-outputpath', type=str, default='tomo-converted-test/', help='output folder path: virtual CT projections')
+parser.add_argument('-k1', type=int, default=100, help='test projection index: for CT')
+
+args, unparsed = parser.parse_known_args()
+if len(unparsed) > 0:
+    print('Unrecognized argument(s): \n%s \nProgram exiting ... ... ' % '\n'.join(unparsed))
+    exit(0)
+
+if os.path.isdir(args.outputpath):
+    shutil.rmtree(args.outputpath)
+os.mkdir(args.outputpath)  # to save output
+
 def main():
-    parser = argparse.ArgumentParser(description='HCT2CT, for conversion from helical CT projection to CT projection for  parallel beam')
-    parser.add_argument('-Np',  type=int, default="3000", help='number of projection per rotation (360 degrees)')
-    parser.add_argument('-Nr', type=int, default=3, help='number of rotation')
-    parser.add_argument('-linear_step', type=float, default=0.001, help='linear step (i.e., traveling distance per projectio), uint of mm')
-    parser.add_argument('-pixel_size',  type=float, default=0.013, help='pixel size, uint of mm')
-    parser.add_argument('-inputpath', type=str, default='tomo/', help='input folder path: HCT projections')
-    parser.add_argument('-outputpath', type=str, default='tomo-converted/', help='output folder path: virtual CT projections')
-    parser.add_argument('-mispixels', type=int, default=18, help='allow to maunally correct the mispixels horizontally, uint of pixel')
-    parser.add_argument('-k1', type=int, default=10, help='test projection index: for CT')
-    
-    args, unparsed = parser.parse_known_args()
-    if len(unparsed) > 0:
-        print('Unrecognized argument(s): \n%s \nProgram exiting ... ... ' % '\n'.join(unparsed))
-        exit(0)
-    
-    if os.path.isdir(args.outputpath):
-        shutil.rmtree(args.outputpath)
-    os.mkdir(args.outputpath) # to save output
-    
     time_git_st = time.time()
-    Convertedproj = hct2ct(args.Np, args.Nr, args.linear_step, args.pixel_size, args.inputpath, args.mispixels, args.k1)
+    imprefix = 'proj_'
+    imzeropad = 5
+    Convertedproj = hct2ct(args.Np, args.Nr, args.linear_step, args.pixel_size, args.inputpath, args.k1, imprefix, imzeropad)
     projname = args.outputpath + "tomo_" + str("%05d" % (args.k1)) + ".tif"
     tifffile.imsave(projname, Convertedproj)  # save an image from a result
 
     print('Elapse: %.4fs' % (time.time() - time_git_st))
     print("It's done!")
+
 
 if __name__ in ['__builtin__', '__main__']:
     main()
