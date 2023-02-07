810bfbd9d05e500752ff34d7162e09e47db14f39
Xiaoman896 <73009207+Xiaoman896@users.noreply.github.com>
2023-02-07 14:55:36 -0600

    Update hct2ct.py

--- hct2ct.py
+++ hct2ct.py
@@ -2,13 +2,12 @@
 # Author: Xiaoman Duan
 # Contact: xid896@usask.ca
 # Date: 2022-12-07
-# Latest revised  date: 2023-01-18
-
+# Latest revised  date: 2023-02-07
 import numpy as np
 import tifffile
+import os
 
-
-def hct2ct(Np, Nr, linear_step, pixel_size, path, mispixels, k1):
+def hct2ct(Np, Nr, linear_step, pixel_size, path, k1, imprefix, imzeropad):
     print("Generating virtual projection of --->",k1)
     ## k1 --> k
     k = np.zeros(Nr * 2, dtype = np.int16)
@@ -16,11 +15,10 @@ def hct2ct(Np, Nr, linear_step, pixel_size, path, mispixels, k1):
         k[i] = k1 + i * Np / 2
     ## Read HCT projections
     Usedproj =  []
-    for i in range (Nr*2):
-        Rawproj = tifffile.Tifffile (os.path. join(path, imprefix+ str(k[i]).zfill(imzeropad)+".tif")).asarray()
+    for i in range (Nr * 2):
+        Rawproj = tifffile.TiffFile(os.path.join(path, imprefix + str(k[i]).zfill(imzeropad)+".tif")).asarray()
         Rawproj = np.flipud(Rawproj)
-        Usedproj.append (Rawproj)
-
+        Usedproj.append(Rawproj)
     Usedproj = np.array(Usedproj)
     D_travel = np.floor(k * linear_step / pixel_size) ## unit of pixels
     w = k * linear_step / pixel_size - D_travel
@@ -35,8 +33,18 @@ def hct2ct(Np, Nr, linear_step, pixel_size, path, mispixels, k1):
             else:
                 for j in range(np.shape(Rawproj)[1]):
                     j1 = np.shape(Rawproj)[1] - 1 - j
+                    ## automatically find mispixels
+                    if 'mispixels' not in locals().keys():
+                        Std=[]
+                        for mispixels in range(50):
+                            I_current = np.fliplr(Usedproj[kk,:])
+                            I1 = I_current[i, range(np.shape(Rawproj)[1] + 1 - round(3 / 4 * np.shape(Rawproj)[1]) - mispixels, np.shape(Rawproj)[1] + 1 - round(1 / 4 * np.shape(Rawproj)[1]) - mispixels, 1)]
+                            I2 = I_virtal[i1, round((1 / 4 * np.shape(Rawproj)[1])):round((3 / 4 * np.shape(Rawproj)[1]))]
+                            Std.append(sum(abs(I1 - I2)))
+                        mispixels = Std.index(min(Std))-1
+
                     if ((j1 + mispixels) >= 0) & ((j1 + mispixels) < np.shape(Rawproj)[1]):
-                        I_virtal[i1][j1 + mispixels] = w[kk] * Usedproj[kk,i, j] + (1 - w[kk]) * Usedproj[kk,i + 1, j]
+                            I_virtal[i1][j1 + mispixels] = w[kk] * Usedproj[kk,i, j] + (1 - w[kk]) * Usedproj[kk,i + 1, j]
 
     I_virtal = np.flipud(I_virtal.astype(np.uint16))
 
