9c8ac09b1d711b0e1a610b436ad3a76b5c2ccd16
Xiaoman896 <73009207+Xiaoman896@users.noreply.github.com>
2023-02-07 14:55:10 -0600

    Update HCTconvert.py

--- HCTconvert.py
+++ HCTconvert.py
@@ -2,7 +2,7 @@
 # Author: Xiaoman Duan
 # Contact: xid896@usask.ca
 # Create date: 2022-12-07
-# Last edit date: 2023-01-06
+# Last edit date: 2023-02-07
 
 import shutil
 import os, time
@@ -15,8 +15,7 @@ parser.add_argument('-Nr', type=int, default=3, help='number of rotations')
 parser.add_argument('-linear_step', type=float, default=0.001, help='linear step (i.e., traveling distance per projectio), uint of mm')
 parser.add_argument('-pixel_size', type=float, default=0.013, help='pixel size, uint of mm')
 parser.add_argument('-inputpath', type=str, default='tomo/', help='input folder path: HCT projections')
-parser.add_argument('-outputpath', type=str, default='tomo-converted/', help='output folder path: virtual CT projections')
-parser.add_argument('-mispixels', type=int, default=18, help='allow to maunally correct the mispixels horizontally because the rotation center may be deviate from the detector center, uint of pixel')
+parser.add_argument('-outputpath', type=str, default='tomo-converted-test/', help='output folder path: virtual CT projections')
 
 args, unparsed = parser.parse_known_args()
 if len(unparsed) > 0:
@@ -31,7 +30,9 @@ def main():
 
     for k1 in range(int(args.Np / 2)):
         time_git_st = time.time()
-        Convertedproj = hct2ct(args.Np, args.Nr, args.linear_step, args.pixel_size, args.inputpath, args.mispixels, k1)
+        imprefix = 'proj_'
+        imzeropad = 5
+        Convertedproj = hct2ct(args.Np, args.Nr, args.linear_step, args.pixel_size, args.inputpath, k1, imprefix, imzeropad)
         projname = args.outputpath + "tomo_" + str("%05d" % (k1)) + ".tif"
         tifffile.imsave(projname, Convertedproj)  # save an image from a result
         print('Elapse: %.4fs' % (time.time() - time_git_st))
